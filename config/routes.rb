4d6258988a31aa4a4789a8aacf92941cb6ecc481
mom0tomo <momo76w@gmail.com>
2018-09-06 08:32:24 +0000

    不要なルーティングを削除

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
-  resources :users, only: [:show, :create, :destroy]
+  resources :users, only: [:show, :create]
   get 'signup', to: 'users#new'
 
   resources :tasks
